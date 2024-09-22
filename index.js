34a0b7493abd62b2a41440e204fa8f5889ff46a3
Cristinapdlcruz <cristinapdlcruz@gmail.com>
2024-09-23 00:58:35 +0200

    updates

--- index.js
+++ index.js
@@ -112,6 +112,58 @@ const ZAPATILLAS = [
     precio: 140.0
   }
 ]
+const modelos = []
+const marcas = []
+
+const fillFiltro = (zzapas) => {
+  modelos.splice(0)
+  marcas.splice(0)
+
+  for (const zapa of zzapas) {
+    if (!modelos.includes(zapa.modelo)) {
+      modelos.push(zapa.modelo)
+    }
+
+    if (!marcas.includes(zapa.marca)) {
+      marcas.push(zapa.marca)
+    }
+  }
+}
+fillFiltro(ZAPATILLAS)
+
+const seleccionarModelo = () => {
+  const divFiltros = document.querySelector('#filtros')
+  const selectModelo = document.createElement('select')
+  const selectMarca = document.createElement('select')
+
+  const agregarOpciones = (array, selectElement) => {
+    selectElement.innerHTML = ''
+    for (const item of array) {
+      const option = document.createElement('option')
+      option.value = item
+      option.textContent = item
+
+      selectElement.appendChild(option)
+    }
+  }
+
+  agregarOpciones(marcas, selectMarca)
+
+  selectMarca.addEventListener('change', function () {
+    const marcaSeleccionada = selectMarca.value
+
+    const modelosDeMarca = ZAPATILLAS.filter(
+      (zapa) => zapa.marca === marcaSeleccionada
+    ).map((zapa) => zapa.modelo)
+
+    const modelosUnicos = [...new Set(modelosDeMarca)]
+
+    agregarOpciones(modelosDeMarca, selectModelo)
+  })
+
+  divFiltros.appendChild(selectMarca)
+  divFiltros.appendChild(selectModelo)
+}
 const printZapas = (zapas) => {
   const divZapas = document.querySelector('#zapatillas')
   divZapas.innerHTML = ''
@@ -120,20 +172,24 @@ const printZapas = (zapas) => {
     const divImg = document.createElement('div')
     const img = document.createElement('img')
     const nombre = document.createElement('h3')
+    const modelo = document.createElement('h4')
     const precio = document.createElement('p')
 
     divZapa.className = 'flex-container'
     divImg.classList = 'img.container'
     img.src = zapa.imagen
     nombre.textContent = zapa.marca
+    modelo.textContent = zapa.modelo
     precio.textContent = `${zapa.precio}€`
 
     divZapa.appendChild(divImg)
     divImg.appendChild(img)
     divZapa.appendChild(nombre)
+    divZapa.appendChild(modelo)
     divZapa.appendChild(precio)
     divZapas.appendChild(divZapa)
   }
 }
 
 printZapas(ZAPATILLAS)
+seleccionarModelo()
