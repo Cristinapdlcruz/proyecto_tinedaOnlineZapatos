75b8ffe4ad25eaa4c55c84257bb1911afe521cd3
Cristinapdlcruz <cristinapdlcruz@gmail.com>
2024-09-25 12:19:46 +0200

    project finale

--- index.js
+++ index.js
@@ -112,84 +112,238 @@ const ZAPATILLAS = [
     precio: 140.0
   }
 ]
-const modelos = []
-const marcas = []
 
-const fillFiltro = (zzapas) => {
-  modelos.splice(0)
-  marcas.splice(0)
+const printHeader = (header) => {
+  const divHeader = document.querySelector('#headerPage')
+  const logo = document.createElement('img')
+  const logoLink = document.createElement('a')
 
-  for (const zapa of zzapas) {
-    if (!modelos.includes(zapa.modelo)) {
-      modelos.push(zapa.modelo)
-    }
+  const OptionSearch = document.createElement('section')
+  const hombre = document.createElement('a')
+  const mujer = document.createElement('a')
+  const niños = document.createElement('a')
+  const ofertas = document.createElement('a')
 
-    if (!marcas.includes(zapa.marca)) {
-      marcas.push(zapa.marca)
-    }
-  }
+  const OptionUser = document.createElement('section')
+  const filtro = document.createElement('img')
+  const cesta = document.createElement('img')
+  const user = document.createElement('img')
+  const filtroLink = document.createElement('a')
+  const cestaLink = document.createElement('a')
+  const userLink = document.createElement('a')
+
+  logo.src = './assets/JDlogo.png'
+  logo.alt = 'logo_main'
+  logo.className = 'logo'
+  logoLink.href = '#'
+
+  OptionSearch.className = 'header-options flex-container'
+  hombre.href = '#'
+  mujer.href = '#'
+  niños.href = '#'
+  ofertas.href = '#'
+  hombre.textContent = 'Hombre'
+  mujer.textContent = 'Mujer'
+  niños.textContent = 'Niños'
+  ofertas.textContent = 'Ofertas'
+
+  OptionUser.className = 'user-options flex-container'
+  logo.src = './assets/JDlogo.png'
+  logo.alt = 'logo_main'
+  logo.className = 'logo'
+  logoLink.href = '#'
+
+  filtro.src = './assets/iconfilter.png'
+  filtro.alt = 'filtro_main'
+  filtro.className = 'filtro'
+  filtroLink.href = '#'
+  cesta.src = './assets/iconshopping.png'
+  cesta.alt = 'cesta_main'
+  cesta.className = 'cesta'
+  cestaLink.href = '#'
+  user.src = './assets/iconuser.png'
+  user.alt = 'user_main'
+  user.className = 'user'
+  userLink.href = '#'
+
+  logoLink.appendChild(logo)
+  divHeader.appendChild(logoLink)
+  divHeader.appendChild(OptionSearch)
+  OptionSearch.appendChild(hombre)
+  OptionSearch.appendChild(mujer)
+  OptionSearch.appendChild(niños)
+  OptionSearch.appendChild(ofertas)
+
+  divHeader.appendChild(OptionUser)
+  filtroLink.appendChild(filtro)
+  OptionUser.appendChild(filtroLink)
+  cestaLink.appendChild(cesta)
+  OptionUser.appendChild(cestaLink)
+  userLink.appendChild(user)
+  OptionUser.appendChild(userLink)
 }
-fillFiltro(ZAPATILLAS)
+printHeader()
 
-const seleccionarModelo = () => {
-  const divFiltros = document.querySelector('#filtros')
-  const selectModelo = document.createElement('select')
-  const selectMarca = document.createElement('select')
+const cover = () => {
+  const coverMain = document.querySelector('#JDstyle')
+  const imageCover = document.createElement('img')
+  coverMain.src = './assets/cover.png'
+  coverMain.alt = 'cover_main'
+  coverMain.className = 'cover'
 
-  const agregarOpciones = (array, selectElement) => {
-    selectElement.innerHTML = ''
-    for (const item of array) {
-      const option = document.createElement('option')
-      option.value = item
-      option.textContent = item
+  coverMain.appendChild(imageCover)
+}
+cover()
 
-      selectElement.appendChild(option)
-    }
-  }
+const filtroOption = () => {
+  const divFiltro = document.querySelector('#filtros')
+  divFiltro.innerHTML = ''
+  const filtroMarca = document.createElement('input')
+  const filtroModelo = document.createElement('input')
+  const botonBuscar = document.createElement('button')
+  const botonLimpiar = document.createElement('button')
+  const arrowImg = document.createElement('img')
 
-  agregarOpciones(marcas, selectMarca)
+  filtroMarca.className = 'filtroMarca'
+  filtroMarca.placeholder = 'Marca'
 
-  selectMarca.addEventListener('change', function () {
-    const marcaSeleccionada = selectMarca.value
+  filtroModelo.className = 'filtroModelo'
+  filtroModelo.placeholder = 'Modelo'
 
-    const modelosDeMarca = ZAPATILLAS.filter(
-      (zapa) => zapa.marca === marcaSeleccionada
-    ).map((zapa) => zapa.modelo)
+  botonBuscar.className = 'buscarBtn'
+  botonBuscar.textContent = 'Buscar'
+  botonLimpiar.className = 'limpiarBtn'
+  botonLimpiar.textContent = 'Limpiar'
 
-    const modelosUnicos = [...new Set(modelosDeMarca)]
+  arrowImg.src = './assets/arrowicon.png'
+  arrowImg.alt = 'Arrow'
+  arrowImg.className = 'arrowImg'
 
-    agregarOpciones(modelosDeMarca, selectModelo)
+  divFiltro.appendChild(filtroMarca)
+  divFiltro.appendChild(filtroModelo)
+  divFiltro.appendChild(botonBuscar)
+  divFiltro.appendChild(botonLimpiar)
+  divFiltro.appendChild(arrowImg)
+
+  filtroMarca.style.display = 'none'
+  filtroModelo.style.display = 'none'
+  botonBuscar.style.display = 'none'
+  botonLimpiar.style.display = 'none'
+
+  arrowImg.addEventListener('click', () => {
+    const isVisible = filtroMarca.style.display === 'block'
+    filtroMarca.style.display = isVisible ? 'none' : 'block'
+    filtroModelo.style.display = isVisible ? 'none' : 'block'
+    botonBuscar.style.display = isVisible ? 'none' : 'block'
+    botonLimpiar.style.display = isVisible ? 'none' : 'block'
   })
-
-  divFiltros.appendChild(selectMarca)
-  divFiltros.appendChild(selectModelo)
 }
+filtroOption()
+
+const filtrarZapas = () => {
+  const marca = document.querySelector('.filtroMarca').value.toLowerCase()
+  const modelo = document.querySelector('.filtroModelo').value.toLowerCase()
+
+  const zapasFiltradas = ZAPATILLAS.filter((zapa) => {
+    return (
+      (marca === '' || zapa.marca.toLowerCase().includes(marca)) &&
+      (modelo === '' || zapa.modelo.toLowerCase().includes(modelo))
+    )
+  })
+  printZapas(zapasFiltradas)
+}
+document.querySelector('.buscarBtn').addEventListener('click', filtrarZapas)
+document.querySelector('.limpiarBtn').addEventListener('click', () => {
+  document.querySelector('.filtroMarca').value = ''
+  document.querySelector('.filtroModelo').value = ''
+  printZapas(ZAPATILLAS)
+})
+
 const printZapas = (zapas) => {
   const divZapas = document.querySelector('#zapatillas')
   divZapas.innerHTML = ''
   for (const zapa of zapas) {
     const divZapa = document.createElement('div')
     const divImg = document.createElement('div')
+    const divInfo = document.createElement('div')
     const img = document.createElement('img')
     const nombre = document.createElement('h3')
     const modelo = document.createElement('h4')
     const precio = document.createElement('p')
+    const botonComprar = document.createElement('button')
 
     divZapa.className = 'flex-container'
     divImg.classList = 'img.container'
+    divInfo.className = 'info'
     img.src = zapa.imagen
     nombre.textContent = zapa.marca
     modelo.textContent = zapa.modelo
     precio.textContent = `${zapa.precio}€`
+    botonComprar.className = 'comprarBtn'
+    botonComprar.textContent = 'Comprar'
 
     divZapa.appendChild(divImg)
     divImg.appendChild(img)
-    divZapa.appendChild(nombre)
-    divZapa.appendChild(modelo)
-    divZapa.appendChild(precio)
+    divZapa.appendChild(divInfo)
+    divInfo.appendChild(nombre)
+    divInfo.appendChild(modelo)
+    divInfo.appendChild(precio)
+    divInfo.appendChild(botonComprar)
     divZapas.appendChild(divZapa)
   }
 }
-
 printZapas(ZAPATILLAS)
-seleccionarModelo()
+
+const printFooter = () => {
+  const footer = document.querySelector('#footer')
+  footer.className = 'flex-container'
+  const columns = [
+    {
+      title: 'Compra con JD',
+      paragraphs: [
+        'Guía de tallas',
+        'PBuscador de tallas',
+        'Descuento estudiantes',
+        'Calendario lanzamientos',
+        'Inscribete a JDX',
+        'JD Blog'
+      ]
+    },
+    {
+      title: 'Atención al cliente',
+      paragraphs: [
+        'Preguntas Frecuentes',
+        'Envíos y devoluciones',
+        'Seguimiento de envío',
+        'Contacto'
+      ]
+    },
+    {
+      title: 'Aviso Legal',
+      paragraphs: [
+        'Términos y condiciones',
+        'Promociones y condiciones',
+        'Política de privacidad',
+        'Política de cookies',
+        'Ajustes de cookies',
+        'Accesibilidad'
+      ]
+    }
+  ]
+
+  columns.forEach((column, index) => {
+    const section = document.createElement('section')
+    section.className = section.className = `${column.title
+      .replace(/\s+/g, '-')
+      .toLowerCase()}`
+
+    for (const paragraph of column.paragraphs) {
+      const p = document.createElement('p')
+      p.textContent = paragraph
+      section.appendChild(p)
+    }
+    footer.appendChild(section)
+  })
+}
+
+printFooter()
